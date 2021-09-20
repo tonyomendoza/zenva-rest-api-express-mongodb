2b7d6d1e7f2b6f3247a2de34c99d3ae0fb1b2e94
tonyomendoza <mendozatony@outlook.com>
2021-09-20 16:58:17 -0700

    Lesson 3 Making our first requests

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-import nodemon from 'nodemon';
+import express from 'express';
+import parser from 'body-parser';
 
-console.log('hello world')
+const app = new express();
+app.use(parser.urlencoded({
+    extended: false
+}));
+
+app.get('/', (req, res) =>{
+    return res.send('Hello world');
+});
+
+app.get('/world', (req, res) =>{
+    return res.send('World');
+});
+
+app.post('/', (req, res) => {
+    return res.send(req.body);
+})
+
+app.listen(process.env.PORT || 3000)
