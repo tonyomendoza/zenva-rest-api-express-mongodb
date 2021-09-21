9bbeb91b018ec5d955fefc443ffa756d0661980b
tonyomendoza <mendozatony@outlook.com>
2021-09-20 22:42:23 -0700

    Lesson 6 Error Handling and Response Codes

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,17 +4,26 @@ const router = Router();
 
 router.get('/', (req, res) =>{
     User.find({}, function(err, users){
+        if(err){
+            return res.status(500).send({err});
+        }
         return res.send(users);
     })
 });
 
 router.post('/', (req, res) => {
     const {username} = req.body;
+    if(!username){
+        return res.send({err: 'Required Fields not found: username'});
+    }
     const newUser = new User({
         username: username
     });
     newUser.save(function(err, model){
-        return res.send(model);
+        if(err){
+            return res.status(400).send({err: err});
+        }
+        return res.status(201).send(model);
     });
 })
 
