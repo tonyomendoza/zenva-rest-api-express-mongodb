b4c8b7ec9a7769939027896b691667d87e6f69b2
tonyomendoza <mendozatony@outlook.com>
2021-09-21 00:20:50 -0700

    Lesson 7 Generating and Securing Passwords

--- models/User.js
+++ models/User.js
@@ -1,9 +1,38 @@
 import { Schema } from 'mongoose';
 import mongoose from '../database/mongodb';
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
-    username: { type:String, required: true, index: {unique:true }}
+    username: { type:String, required: true, index: {unique:true }},
+    password: { type:String, required: true }
 });
+
+function updatePassword(user, next){
+    bcrypt.genSalt(10, function (err, salt){
+        if(err)
+            return next(err);
+
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if (err)
+                return next(err);
+            user.password = hash;
+            next();
+        })
+    })
+}
+
+userSchema.pre('save', function(next){
+    updatePassword(this, next);
+});
+
+userSchema.methods.comparePassword = function(password, next) {
+    bcrypt.compare(password, this.password, function(err, isMatch){
+        if(err)
+            return next(err);
+        return next(null, isMatch);
+    });
+}
+
 const User = mongoose.model('user', userSchema);
 
 export default User;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { restart } from 'nodemon';
 import User from '../../models/User';
 const router = Router();
 
@@ -11,17 +12,40 @@ router.get('/', (req, res) =>{
     })
 });
 
+router.post('/password', (req, res) =>{
+    const {username, password } = req.body;
+    if(!username || !password){
+        return res.send({ err: `Required Fields not found: ${!username ? 'username' : ''}${!password ? 'password':''}`
+        });
+    }
+    User.findOne({username: username}, function(err, userModel){
+        if(err)
+            return res.status(400).send(err);
+
+        if(!userModel)
+            return res.status(400).send({err: 'Cannot find user'});
+
+        return userModel.comparePassword(password, function(err, isMatch){
+            if(err)
+                return res.status(400).send(err);
+            return res.send({correct: isMatch});
+        })
+    });
+});
+
 router.post('/', (req, res) => {
-    const {username} = req.body;
-    if(!username){
-        return res.send({err: 'Required Fields not found: username'});
+    const {username, password } = req.body;
+    if(!username || !password){
+        return res.send({ err: `Required Fields not found: ${!username ? 'username' : ''}${!password ? 'password':''}`
+        });
     }
     const newUser = new User({
-        username: username
+        username: username,
+        password: password
     });
     newUser.save(function(err, model){
         if(err){
-            return res.status(400).send({err: err});
+            return res.status(400).send({err});
         }
         return res.status(201).send(model);
     });
