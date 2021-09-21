1042e3391b5d5efe2cd5e355cf48743547b1e072
tonyomendoza <mendozatony@outlook.com>
2021-09-20 22:32:37 -0700

    Lesson 4 Using Mongodb with Requests

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import express from 'express';
 import parser from 'body-parser';
+import apiRoutes from './routes/api';
+
+require('dotenv').config();
 
 const app = new express();
 app.use(parser.urlencoded({
     extended: false
 }));
 
+app.use('/api', apiRoutes);
+
 app.get('/', (req, res) =>{
     return res.send('Hello world');
 });
-
-app.get('/world', (req, res) =>{
-    return res.send('World');
-});
-
-app.post('/', (req, res) => {
-    return res.send(req.body);
-})
 
 app.listen(process.env.PORT || 3000)
